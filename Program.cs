2ec74fe9df1d72d76b451d41a88e2813f9f697c1
tehtko <moderndsnsam@gmail.com>
2024-01-16 14:34:55 -0500

    Create LinkedListTest

--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
-﻿LinkedList.List<int> mylist = new();
+﻿void LinkedListTest()
+{
+    LinkedList.List<int> mylist = new();
+    mylist.Pop(); // Should do nothing.
+    mylist.Unshift(); // Should do nothing.
 
-mylist.Push(3);
-mylist.Push(5);
-mylist.Push(2);
-mylist.Push(9);
-mylist.Push(6);
-mylist.Push(7);
-mylist.Shift(1);
-mylist.Print();
-Console.WriteLine("\n" + mylist.Pop());
-mylist.Print();
-Console.WriteLine("\n" + mylist.Unshift());
-mylist.Print();
+    // Should result in 3->5->2->9->6->7
+    mylist.Push(3);
+    mylist.Push(5);
+    mylist.Push(2);
+    mylist.Push(9);
+    mylist.Push(6);
+    mylist.Push(7);
+    mylist.Print();
+    Console.WriteLine("\nPopped: " + mylist.Pop()); // 7
+
+    // Should result in 1->3->5->2->9->6
+    mylist.Shift(1);
+    mylist.Print();
+
+    Console.WriteLine("\nUnshifted: " + mylist.Unshift()); // 1
+
+    // Should print 3->5->2->9->6
+    mylist.Print();
+}
+
+LinkedListTest();
